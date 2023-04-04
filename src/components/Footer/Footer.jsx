2e61eb61855172b863b44bbe335ca5f8e9584cda
Aunjrya Fleming <104907548+ParisianMist@users.noreply.github.com>
2023-04-04 16:31:08 -0400

    add framework for footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <p className="footer__text">
+                Copyright © 2023
+            </p>
+            <p className="footer__contact">
+                <a href="" className="footer__contact--link">
+                    Aunjrya Fleming Solutions
+                </a>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,13 +2,16 @@ import './Profile.scss';
 
 //components
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
 const Profile = () => {
     
    
     return (
-        <>
+        <main>
           <Header/>
-        </>
+          <Footer/>
+        </main>
     );
 };
 
